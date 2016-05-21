a6bd107a9e454693a153e46491772747fb399e7c
Fang Lin <fanglin@uw.edu>
2016-05-20 17:34:41 -0700

    refine create user.

--- endorsement/dao/__init__.py
+++ endorsement/dao/__init__.py
@@ -1,12 +1,18 @@
+from django.utils import timezone
 from restclients.dao import PWS_DAO
 from endorsement.models.user import User
 from endorsement.dao.user import get_netid_of_current_user
+from endorsement.dao.pws import get_regid
 
 
 def get_user_model():
     user_netid = get_netid_of_current_user()
+    user_regid = get_regid(user_netid)
 
-    user, created = User.objects.get_or_create(uwnetid=user_netid)
+    user, created = User.objects.get_or_create(
+        uwnetid=user_netid,
+        uwregid=user_regid,
+        defaults={'last_visit': timezone.now()})
 
     return user
 
