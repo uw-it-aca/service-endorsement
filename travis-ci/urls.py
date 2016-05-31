d9ae08544b5435cb7331ebff0c386e7302e6ce61
Fang Lin <fanglin@uw.edu>
2016-05-30 19:49:55 -0700

    Remove django.conf.urls.patterns() as it is deprecated

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = [
+    url(r'^support', include('userservice.urls')),
+    url(r'^logging/', include('django_client_logger.urls')),
     url(r'^', include('endorsement.urls')),
-)
+]
