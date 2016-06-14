ffeccf504ed40e3541fd87c5aea962c9fefeae08
Stephen De Vight <devights@gmail.com>
2016-06-13 22:46:32 -0700

    pep8

--- endorsement/management/commands/statics_to_path.py
+++ endorsement/management/commands/statics_to_path.py
@@ -3,6 +3,7 @@ from django.conf import settings
 # from django.contrib.staticfiles import coll
 from django.core import management
 
+
 class Command(BaseCommand):
     help = 'Collects app\'s static files to a specified path'
     args = "<statics path>"
@@ -11,7 +12,9 @@ class Command(BaseCommand):
         try:
             statics_path = args[0]
             settings.STATIC_ROOT = statics_path
-            management.call_command('collectstatic', interactive=False, verbosity=0)
+            management.call_command('collectstatic',
+                                    interactive=False,
+                                    verbosity=0)
         except IndexError:
             raise CommandError("You must specify a statics path")
         except Exception as ex:
