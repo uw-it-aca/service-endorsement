a5d01ee6ec740ae47f62c7c932a74a44442225f2
Fang Lin <fanglin@uw.edu>
2016-06-14 22:26:19 -0700

    Reactivate page unittest.

--- endorsement/tests.py
+++ endorsement/tests.py
@@ -5,5 +5,5 @@ from endorsement.test.dao.migration import TestMigration
 from endorsement.test.dao.pws import TestPwsDao
 from endorsement.test.dao.user import TestUserDao
 from endorsement.test.dao.uwnetid_subscription_60 import TestNetidSubs60Dao
-# from endorsement.test.views.page import TestPage
+from endorsement.test.views.page import TestPage
 from endorsement.test.views.session import TestLogSession
