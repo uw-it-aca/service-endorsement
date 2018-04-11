83f32fbcbe4e0ae9826f2cc323c0b6c209a3d0ef
mike seibel <mikes@washington.edu>
2018-04-10 17:20:05 -0700

    test for invalid netid

--- endorsement/test/views/test_page.py
+++ endorsement/test/views/test_page.py
@@ -1,5 +1,6 @@
 from endorsement.test.views import require_url, TestViewApi
 from userservice.user import get_original_user
+import re
 
 
 @require_url('home', 'endorsement urls not configured')
@@ -8,9 +9,12 @@ class TestPage(TestViewApi):
     def test_err_cases(self):
         request = self.get_request('/', 'jnone')
         self.assertEqual(get_original_user(request), 'jnone')
-
         response = self.get_response("home")
-        self.assertEqual(response.status_code, 401)
+
+        self.assertEqual(response.status_code, 200)
+        c = re.match('.*<div class="(invalid-endorser)">.*',
+                     response.content, re.DOTALL)
+        self.assertEqual(c.lastindex, 1)
 
     def test_normal_cases(self):
         request = self.get_request('/', 'jstaff')
