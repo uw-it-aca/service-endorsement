bee8204a56cf5e60377fc04e41db0b1e094dd13e
mike seibel <mikes@washington.edu>
2018-06-21 15:59:02 -0700

    bumps uw_nws version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'UW-RestClients-Core<1.0,>=0.9.3',
         'UW-RestClients-PWS<1.0,>=0.6',
         'UW-RestClients-GWS<1.0',
-        'UW-RestClients-UWNetID>=0.8.1,<1.0',
+        'UW-RestClients-UWNetID>=0.8.2,<1.0',
         'UW-RestClients-Django-Utils>=0.6.8,<1.0',
         'Django-Safe-EmailBackend>=0.1,<1.0',
         'UW-Django-SAML2>=0.4',
