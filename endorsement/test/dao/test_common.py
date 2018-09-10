f0fc12a769ff71a45c50cc91382b39393fdc9d7c
mike seibel <mikes@washington.edu>
2018-09-10 16:56:46 -0700

    account for env in test

--- endorsement/test/dao/test_common.py
+++ endorsement/test/dao/test_common.py
@@ -8,4 +8,4 @@ class TestCommonFunctions(TestDao):
     def test_display_datetime(self):
         d = datetime(1993, 12, 10, 11, 30, 30)
         dd = display_datetime(d)
-        self.assertEqual(dd, 'December 10 at 11:30 AM UTC')
+        self.assertEqual(dd[:23], 'December 10 at 11:30 AM')
