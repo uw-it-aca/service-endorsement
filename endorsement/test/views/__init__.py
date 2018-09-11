8ad14ae241f4259f40b80aa04a16709d8fe25c3a
mike seibel <mikes@washington.edu>
2018-09-11 16:11:22 -0700

    add accept view tests

--- endorsement/test/views/__init__.py
+++ endorsement/test/views/__init__.py
@@ -8,9 +8,9 @@ from django.test.client import RequestFactory
 from django.test.utils import override_settings
 
 
-def _missing_url(name):
+def _missing_url(name, kwargs=None):
     try:
-        reverse(name)
+        reverse(name, kwargs=kwargs)
     except Exception as ex:
         print("Ex: {0}".format(ex))
         return True
@@ -18,8 +18,8 @@ def _missing_url(name):
     return False
 
 
-def require_url(url, message):
-    return skipIf(_missing_url(url), message)
+def require_url(url, message, kwargs=None):
+    return skipIf(_missing_url(url, kwargs), message)
 
 
 def get_user(netid):
