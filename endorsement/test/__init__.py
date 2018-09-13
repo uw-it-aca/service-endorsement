147029485cedf151f8e0a3404406d5d6614714a4
mike seibel <mikes@washington.edu>
2018-09-13 13:17:51 -0700

    logout test

--- endorsement/test/__init__.py
+++ endorsement/test/__init__.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import User
+from uw_gws.utilities import fdao_gws_override
+from uw_pws.util import fdao_pws_override
+from uw_uwnetid.util import fdao_uwnetid_override
 
 
 def get_user(username):
