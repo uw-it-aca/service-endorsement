5498cdf4797565192e335202e3fa036b72320c63
mike seibel <mikes@washington.edu>
2019-10-21 16:31:25 -0700

    clean up deploy script

--- travis-ci/deploy.sh
+++ travis-ci/deploy.sh
@@ -11,9 +11,7 @@ elif [ "$TRAVIS_BRANCH" = "master" ]; then
     INSTANCE="prod"
     GCP_PROJECT="uwit-mci-0011"
 else
-    echo "###############################################"
     echo "Branch $TRAVIS_BRANCH not configured for deployment"
-    echo "###############################################"
     exit 1
 fi
 
@@ -24,17 +22,12 @@ HELM_RELEASE=${APP_NAME}-prod-${INSTANCE}
 export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
-echo "##########################"
-echo "DEPLOY $HELM_RELEASE"
-echo "##########################"
+echo "DEPLOYING $HELM_RELEASE in $GCP_PROJECT"
 
-echo "######################################"
 echo "PUSH image $IMAGE_TAG to repository"
 echo -n "$DOCKER_PASS" | docker login --username="$DOCKER_USER" --password-stdin;
 docker push "$IMAGE_TAG";
-echo "######################################"
 
-echo "##########################"
 if [ ! -d $HOME/google-cloud-sdk/bin ]; then
     echo "INSTALL gcloud sdk"
     rm -rf $HOME/google-cloud-sdk
@@ -44,27 +37,20 @@ echo "CONFIGURE gcloud sdk"
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud components update kubectl
 gcloud version
-echo "##########################"
 
-echo "##########################"
-echo "INSTALL helm"
 if [ ! -d $HOME/helm/bin ]; then
-  mkdir $HOME/helm && pushd $HOME/helm 
-  curl -Lso ${HELM_TGZ} ${HELM_URL}/${HELM_TGZ} && tar xzf ${HELM_TGZ}
-  mkdir $HOME/helm/bin && mv ./linux-amd64/helm ./bin/helm
-  popd
+    echo "INSTALL helm"
+    mkdir $HOME/helm && pushd $HOME/helm 
+    curl -Lso ${HELM_TGZ} ${HELM_URL}/${HELM_TGZ} && tar xzf ${HELM_TGZ}
+    mkdir $HOME/helm/bin && mv ./linux-amd64/helm ./bin/helm
+    popd
 fi
+echo "CONFIGURE helm"
 export PATH=$HOME/helm/bin:$PATH
 helm init --client-only;
-echo "##########################"
 
-echo "##########################"
 echo "CLONE chart $HELM_CHART_REPO"
 git clone --depth 1 "$HELM_CHART_REPO" --branch master;
-ls ./django-production-chart
-echo "##########################"
 
-echo "##########################"
-echo "helm upgrade ${APP_NAME}-prod-${INSTANCE}"
+echo "DEPLOY release $HELM_RELEASE"
 echo helm upgrade $HELM_RELEASE ./django-production-chart --install --set commitHash=$COMMIT_HASH -f docker/${INSTANCE}-values.yml --dry-run --debug;
-echo "##########################"
