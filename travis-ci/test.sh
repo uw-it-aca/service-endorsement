9b20e0ebb4d1650da57f9b276533fe870406fcd7
mike seibel <mikes@washington.edu>
2019-10-22 12:34:41 -0700

    travis test script exit status

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -10,24 +10,23 @@ pip install pycodestyle coveralls
 apt-get install -y nodejs npm
 npm install -g jshint
 
+function run_test {
+    echo "##########################"
+    echo "TEST: $1"
+    echo "##########################"
+    eval $1
+    if [ $? != 0 ] ; then exit 1; fi
+}
 
-TEST="pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
-echo TEST: ${TEST}
-eval $TEST
+run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
 
 if [ -d ${DJANGO_APP}/static/${DJANGO_APP}/js ]; then
-    TEST="jshint ${DJANGO_APP}/static/${DJANGO_APP}/js --verbose"
-    echo TEST: ${TEST}
-    eval $TEST
+    run_test "jshint ${DJANGO_APP}/static/${DJANGO_APP}/js --verbose"
 elif [ -d ${DJANGO_APP}/static/js ]; then
-    TEST="jshint ${DJANGO_APP}/static/js --verbose"
-    echo TEST: ${TEST}
-    eval $TEST
+    run_test "jshint ${DJANGO_APP}/static/js --verbose"
 fi
 
-TEST="coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
-echo TEST: ${TEST}
-eval $TEST
+run_test "coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
 
 # put generaged coverage result where it will get processed
 cp .coverage /coverage/.coverage
