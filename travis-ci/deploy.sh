ed25111cf553369e7c8859affe41f4c79e277d84
mike seibel <mikes@washington.edu>
2019-11-12 15:09:44 -0800

    valid branch name

--- travis-ci/deploy.sh
+++ travis-ci/deploy.sh
@@ -7,10 +7,12 @@
 
 # map branch to GCP/MCI project
 if [ "$TRAVIS_BRANCH" = "develop" ]; then
-    INSTANCE="test"
+    APP_INSTANCE="test"
+    FLUX_INSTANCE="dev"
     GCP_PROJECT="uwit-mci-0010"
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
-    INSTANCE="prod"
+    APP_INSTANCE="prod"
+    FLUX_INSTANCE="prod"
     GCP_PROJECT="uwit-mci-0011"
 else
     echo "Branch $TRAVIS_BRANCH not configured for deployment"
@@ -22,17 +24,17 @@ HELM_URL=https://get.helm.sh
 HELM_TGZ=helm-v3.0.0-rc.3-linux-amd64.tar.gz
 HELM_CHART_REPO=https://github.com/uw-it-aca/django-production-chart.git
 HELM_CHART_DIR=django-production-chart
-HELM_RELEASE=${RELEASE_NAME}-prod-${INSTANCE}
+HELM_RELEASE=${RELEASE_NAME}-prod-${APP_INSTANCE}
 
-FLUX_REPO_NAME=gcp-flux-${INSTANCE}
+FLUX_REPO_NAME=gcp-flux-${FLUX_INSTANCE}
 FLUX_REPO_PATH=uw-it-aca/$FLUX_REPO_NAME
 FLUX_REPO=https://${GH_AUTH_TOKEN}@github.com/${FLUX_REPO_PATH}.git
 
-RELEASE_BRANCH=release/${RELEASE_NAME}:${COMMIT_HASH}
+RELEASE_BRANCH=release/${RELEASE_NAME}/${COMMIT_HASH}
 RELEASE_MANIFEST_NAME=${RELEASE_NAME}.yaml
 RELEASE_MANIFEST=${HOME}/${RELEASE_MANIFEST_NAME}
 
-FLUX_RELEASE_MANIFEST=releases/${INSTANCE}/$RELEASE_MANIFEST_NAME
+FLUX_RELEASE_MANIFEST=releases/${FLUX_INSTANCE}/$RELEASE_MANIFEST_NAME
 
 export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
@@ -68,12 +70,10 @@ if [ ! -d $HOME/helm/bin ]; then
     mv ./linux-amd64/helm ./bin/helm
     popd
 fi
-
-echo "CLONE chart $HELM_CHART_REPO"
-git clone --depth 1 "$HELM_CHART_REPO" --branch master $HELM_CHART_DIR >/dev/null 2>&1
+export PATH=${PATH}:${HOME}/helm/bin
 
 echo "GENERATE manifest for release $HELM_RELEASE"
-helm template $HELM_CHART_DIR --set commitHash=$COMMIT_HASH -f docker/${INSTANCE}-values.yml > $RELEASE_MANIFEST
+helm template $HELM_CHART_DIR --set commitHash=$COMMIT_HASH -f docker/${APP_INSTANCE}-values.yml > $RELEASE_MANIFEST
 
 echo "CLONE $FLUX_REPO_PATH, add $FLUX_RELEASE_MANIFEST, create PR"
 git clone --depth 1 "$FLUX_REPO" --branch master $FLUX_REPO_NAME
