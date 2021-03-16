5d6e0633cb5760f720dcec18a01897c52adf83fc
mike seibel <mikes@washington.edu>
2021-03-16 08:24:11 -0700

    grace period per service

--- endorsement/management/commands/expire_endorsees.py
+++ endorsement/management/commands/expire_endorsees.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.core.mail import mail_managers
 from django.template import loader
 from endorsement.policy import endorsements_to_expire
-from endorsement.services import DEFAULT_ENDORSEMENT_GRACETIME
 from endorsement.dao.endorse import clear_endorsement
 import logging
 import urllib3
@@ -15,22 +14,18 @@ class Command(BaseCommand):
     help = 'expire endorsements and alert managers'
 
     # actual expiration happens after one year
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--gracetime', dest='gracetime',
-            default=DEFAULT_ENDORSEMENT_GRACETIME, type=int,
-            help='expiration grace period in days, default {} days'.format(
-                DEFAULT_ENDORSEMENT_GRACETIME))
-
     def handle(self, *args, **options):
         urllib3.disable_warnings()
-        gracetime = options.get('gracetime', DEFAULT_ENDORSEMENT_GRACETIME)
-        endorsements = endorsements_to_expire(gracetime)
+        endorsements = endorsements_to_expire()
 
         if len(endorsements):
             for e in endorsements:
                 try:
+                    logger.info(
+                        "Expired endorsement {} for {} by {}".format(
+                            e.category_code, e.endorsee.netid,
+                            e.endorser.netid))
+
                     clear_endorsement(e)
                 except Exception as ex:
                     logger.error(
