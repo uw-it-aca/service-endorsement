09722608258d7052d0e14810292a0814edc1ac3d
mike seibel <mikes@washington.edu>
2021-09-25 07:41:29 -0700

    test reflect override group adjustment

--- endorsement/test/views/test_admin.py
+++ endorsement/test/views/test_admin.py
@@ -1,16 +1,30 @@
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
-from django.test import override_settings
 from endorsement.test.views import TestViewApi
 from django.urls import reverse
 from endorsement.userservice_validation import can_override_user
+from endorsement.views.support.endorsee_search import EndorseeSearch
+from endorsement.views.support.endorser_search import EndorserSearch
 
 
-@override_settings(
-    MOCK_SAML_ATTRIBUTES={
-        'uwnetid': ['jstaff'],
-        'isMemberOf': ['u_acadev_provision_admin']})
 class TestValidAdmin(TestViewApi):
     def test_valid_override(self):
         request = self.get_request(reverse('userservice_override'), 'jstaff')
         self.assertTrue(can_override_user(request))
+
+    def test_invalid_override(self):
+        request = self.get_request(reverse('userservice_override'), 'javerage')
+        self.assertFalse(can_override_user(request))
+
+        request = self.get_request(reverse('userservice_override'), 'foo@bar')
+        self.assertFalse(can_override_user(request))
+
+    def test_valid_endorsee_search(self):
+        request = self.get_request(reverse('endorsee_search'), 'jstaff')
+        response = EndorseeSearch.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_endorser_search(self):
+        request = self.get_request(reverse('endorser_search'), 'jstaff')
+        response = EndorserSearch.as_view()(request)
+        self.assertEqual(response.status_code, 200)
--- endorsement/test/views/test_support.py
+++ endorsement/test/views/test_support.py
@@ -8,29 +8,6 @@ from endorsement.views.support.endorsee_search import EndorseeSearch
 from endorsement.views.support.endorser_search import EndorserSearch
 
 
-class TestValidAdmin(TestViewApi):
-    def test_valid_override(self):
-        request = self.get_request(reverse('userservice_override'), 'jstaff')
-        self.assertTrue(can_override_user(request))
-
-    def test_invalid_override(self):
-        request = self.get_request(reverse('userservice_override'), 'javerage')
-        self.assertFalse(can_override_user(request))
-
-        request = self.get_request(reverse('userservice_override'), 'foo@bar')
-        self.assertFalse(can_override_user(request))
-
-    def test_valid_endorsee_search(self):
-        request = self.get_request(reverse('endorsee_search'), 'jstaff')
-        response = EndorseeSearch.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_valid_endorser_search(self):
-        request = self.get_request(reverse('endorser_search'), 'jstaff')
-        response = EndorserSearch.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-
-
 @override_settings(
     MOCK_SAML_ATTRIBUTES={
         'uwnetid': ['jstaff'],
@@ -38,7 +15,7 @@ class TestValidAdmin(TestViewApi):
 class TestValidSupport(TestViewApi):
     def test_invalid_override(self):
         request = self.get_request(reverse('userservice_override'), 'jstaff')
-        self.assertFalse(can_override_user(request))
+        self.assertTrue(can_override_user(request))
 
     def test_valid_endorsee_search(self):
         request = self.get_request(reverse('endorsee_search'), 'jstaff')
