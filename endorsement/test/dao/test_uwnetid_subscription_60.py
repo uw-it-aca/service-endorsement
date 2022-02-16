09dee64952c49225793315033f00f7479ec0ca48
mike seibel <mikes@washington.edu>
2022-02-16 15:01:50 -0800

    license date

--- endorsement/test/dao/test_uwnetid_subscription_60.py
+++ endorsement/test/dao/test_uwnetid_subscription_60.py
@@ -1,3 +1,6 @@
+# Copyright 2022 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 # Copyright 2021 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 from endorsement.dao.uwnetid_subscription_60 import is_valid_endorsee,\
