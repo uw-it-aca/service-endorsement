6d8212b84505bbe7cfa25bfa0a4e0c55d2a38e3a
mike seibel <mikes@washington.edu>
2022-04-05 14:52:59 -0700

    shared netid legacy test method in base class

--- endorsement/services/__init__.py
+++ endorsement/services/__init__.py
@@ -88,21 +88,15 @@ class EndorsementServiceBase(ABC):
            - roles: list of supported roles, or '*' to indicate any
            - types: list of supported types, or '*' to indicate any
            - excluded_categories: list of excluded category numbers
-           - allow_existing_endorsement: boolean to grandfather existing
-           - allow_any_previous_endorsement: boolean to grandfather existing
         """
         return None
 
     @property
     def supports_shared_netids(self):
         return (self.shared_params
-                and ((self.shared_params.get('roles', None)
-                      or self.shared_params.get('types', None)
-                      or self.shared_params.get('excluded_categories', None))
-                     or self.shared_params.get(
-                         'allow_existing_endorsement', False)
-                     or self.shared_params.get(
-                         'allow_any_previous_endorsement', False)))
+                and (self.shared_params.get('roles', None)
+                     or self.shared_params.get('types', None)
+                     or self.shared_params.get('excluded_categories', None)))
 
     @property
     def category_name(self):
@@ -140,18 +134,21 @@ class EndorsementServiceBase(ABC):
 
     def valid_supported_netid(self, resource, endorser):
         """
-
         Based on roles and types associated with shared netids that are
         eligible for endorsement as well as associated categories that
         exclude otherwise eligible netids.
         """
         return (self.supports_shared_netids
-                and ((self.valid_supported_role(resource)
-                      and self.valid_supported_type(resource)
-                      and not self.invalid_supported_category(resource))
-                     or self.valid_existing_endorsement(resource, endorser)
-                     or (self.valid_ever_endorsed(resource)
-                         and not self.invalid_supported_category(resource))))
+                and (self.valid_shared_netid(resource)
+                     or self.valid_legacy_shared_netid(resource, endorser)))
+
+    def valid_shared_netid(self, resource):
+        return (self.valid_supported_role(resource)
+                and self.valid_supported_type(resource)
+                and not self.invalid_supported_category(resource))
+
+    def valid_legacy_shared_netid(self, resource, endorser):
+        return False
 
     def valid_supported_role(self, resource):
         """
@@ -202,32 +199,6 @@ class EndorsementServiceBase(ABC):
 
         return shared_netid_has_category(
             supported.name, categories) if len(categories) else False
-
-    def valid_existing_endorsement(self, resource, endorser):
-        try:
-            return (self.shared_params['allow_existing_endorsement']
-                    and self.get_endorsement(
-                        endorser, Endorsee.objects.get(
-                            netid=resource.name)) is not None)
-        except (KeyError,
-                NoEndorsementException,
-                UnrecognizedUWNetid,
-                Endorsee.DoesNotExist):
-            return False
-
-    def valid_ever_endorsed(self, resource):
-        try:
-            endorsee = Endorsee.objects.get(netid=resource.name)
-            # reach around the curtain to include revoked records
-            return (self.shared_params['allow_any_previous_endorsement']
-                    and EndorsementRecord.objects.filter(
-                            category_code=self.category_code,
-                            endorsee=endorsee).count() > 0)
-        except (KeyError,
-                NoEndorsementException,
-                UnrecognizedUWNetid,
-                Endorsee.DoesNotExist):
-            return False
 
     def is_permitted(self, endorser, endorsee):
         try:
