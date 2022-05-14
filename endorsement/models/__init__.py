1c036829ddf9e5bf5e2676b701cd52f342960623
mike seibel <mikes@washington.edu>
2022-05-14 10:51:01 -0700

    rename models core.py

--- endorsement/models/__init__.py
+++ endorsement/models/__init__.py
@@ -1,4 +1,4 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from endorsement.models.core import Endorser, Endorsee, EndorsementRecord
+from endorsement.models.endorsement import Endorser, Endorsee, EndorsementRecord
