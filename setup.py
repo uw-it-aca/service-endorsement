9ecfe174ac83d44319a38cf4869ec7c9b1c6ddaa
mike seibel <mikes@washington.edu>
2025-03-04 13:17:29 -0800

    msca version, housekeeping branch

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'UW-RestClients-UWNetID~=1.1.2',
         'UW-RestClients-Django-Utils~=2.1.5',
         'UW-RestClients-ITBill~=0.1',
-        'UW-RestClients-MSCA~=0.1',
+        'UW-RestClients-MSCA~=0.1.2',
         'Django-Safe-EmailBackend~=1.2',
         'UW-Django-SAML2>=1.3.8,<2.0',
         'django-pyscss',
