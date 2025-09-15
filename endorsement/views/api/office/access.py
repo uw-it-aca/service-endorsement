9363940a921332772c921c59013a8fddecaf2308
mike seibel <mikes@washington.edu>
2025-09-15 15:54:37 -0700

    missing format specifier, error logging

--- endorsement/views/api/office/access.py
+++ endorsement/views/api/office/access.py
@@ -88,6 +88,7 @@ class Access(RESTDispatch):
             access = store_access(
                 accessee, accessor, access_type, acted_as)
         except DataFailureException as ex:
+            logger.error(f"set/revoke access error ({ex.status}): {ex.msg}")
             return self.error_response(ex.status, message=ex.msg)
 
         return self.json_response(access.json_data())
