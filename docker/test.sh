547d12b08d99e096ffcc715e9dd8a782487be0ab
mike seibel <mikes@washington.edu>
2026-08-31 16:11:01 -0700

    remove pycodestyle

--- docker/test.sh
+++ docker/test.sh
@@ -16,8 +16,6 @@ function run_test {
     eval $1
 }
 
-run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
-
 if [ -d ${DJANGO_APP}/static/${DJANGO_APP}/js ]; then
     run_test "jshint ${DJANGO_APP}/static/${DJANGO_APP}/js --verbose"
 elif [ -d ${DJANGO_APP}/static/js ]; then
